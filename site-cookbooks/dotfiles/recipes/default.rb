f430185ac25b44d8a7b191d222d519bf0e5f3d78
ymgyt <yamaguchi7073xtt@gmail.com>
2018-05-05 12:44:49 +0900

    [add] zsh completion
    dotfiles recipeに作成, zshの設定が増えてきたらrecipe切る

--- site-cookbooks/dotfiles/recipes/default.rb
+++ site-cookbooks/dotfiles/recipes/default.rb
@@ -4,6 +4,22 @@
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
 
+user = "#{ENV['ME']}"
+
+directory 'zsh' do
+  path "#{ENV['HOME']}/.zsh/completion"
+  recursive true
+  owner user
+  action :create
+end
+
+cookbook_file 'docker_completion' do
+  path "#{ENV['HOME']}/.zsh/completion/_docker"
+  source "zsh_docker_completion"
+  owner user
+  action :create
+end
+
 template 'zshrc' do
   path "#{ENV['HOME']}/.zshrc"
   source 'zshrc.erb'
